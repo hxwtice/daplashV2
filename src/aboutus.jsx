2dfbc2f01f1f6efa59c982d0eb24a82b79759c80
aditi <aditi.khataniar@gmail.com>
2023-07-11 00:10:59 +0530

    responsive

--- src/aboutus.jsx
+++ src/aboutus.jsx
@@ -4,8 +4,8 @@ export function AboutUs()
 {
     return(
         <>
-        <div className="about-us" id="aboutus">
-            <div className="about-title">About Us</div>
+        <div className="about-us">
+            <div className="about-title" id="aboutus">About Us</div>
             <div className="about-content-1"><span className="about-name">daplash</span>, it's an ideology of supreme marketing by the medium of narrative-based content.</div> 
             <div className="about-content-2"><span className="about-name">daplash</span> consists of experienced and talented digital marketers, UI/UX designers, and editors who are passionate about helping businesses succeed online. We are a collaborative team, and we are always willing to share our knowledge and expertise.</div>
         </div>
