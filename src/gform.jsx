9b288ad44d2669fe4852abfe8de42a33436d004b
aditi <aditi.khataniar@gmail.com>
2023-07-12 01:17:36 +0530

    final touch

--- src/gform.jsx
+++ src/gform.jsx
@@ -4,7 +4,7 @@ export function Form(){
     return(
         <>
         <div className="form">
-            <div className="greet">Get in touch with us :</div>
+            <div className="greet">Get in touch with us:</div>
             <div className="gform">
                 <div className="button"><a href="https://docs.google.com/forms/d/e/1FAIpQLScS6Kpk9lmp7CrpuUa5VBpW0rNE5NQRxNMAtbaWykFl8k0cAg/viewform" target='_blank' className="button-2"><button className="button-1">Schedule a Call</button></a></div>
                 <div className="min">Takes less than one minute.</div>
