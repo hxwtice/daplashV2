a8916205d55d9467099d835f3572c25d29357c54
hxwtice <113427045+hxwtice@users.noreply.github.com>
2023-07-15 03:27:07 +0530

    Update footer.jsx
    updated email link

--- src/footer.jsx
+++ src/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 export function Footer(){
     return(
         <>
-        <div className="footer">Contact: <a href="https://mail.google.com/mail/?view=cm&fs=1&to=daplash1@gmail.com" className="footer-1">hello@daplash.com</a></div>
+        <div className="footer">Contact: <a href="https://mail.google.com/mail/u/0/?fs=1&to=hello@daplash.com&tf=cm" className="footer-1">hello@daplash.com</a></div>
         </>
     )
 }
